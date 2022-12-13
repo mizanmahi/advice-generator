b29c7f2a9fe295a774f72b05087e9419db60e816
mizanmahi <mizanmahi24@gmail.com>
2022-12-13 13:41:10 +0600

    add: some utility classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import Advice from './components/Advice'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  // useEffect(() => {
+  //   document.documentElement.classList.add('dark')
+  // }, [])
 
   return (
-    <div className="h-screen w-full bg-darkBlue flex items-center justify-center px-4">
+    <div className="h-screen w-full dark:bg-darkBlue bg-slate-100 flex items-center justify-center px-4">
         <Advice />
     </div>
   )
--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -35,24 +35,26 @@ const Advice = () => {
    }, []);
 
    return (
-      <div className='bg-darkGrayishBlue w-100 max-w-md p-6 sm:p-10 rounded-lg text-center text-lightCyan flex flex-col gap-4 relative'>
-         <p className='text-sm text-neonGreen tracking-[.25em] uppercase'>
+      <div className='dark:bg-darkGrayishBlue bg-white shadow-sm  w-100 max-w-md p-6 sm:p-10 rounded-lg text-center text-lightCyan flex flex-col gap-4 relative'>
+         <p className='text-sm dark:text-neonGreen text-darkBlue tracking-[.25em] uppercase font-bold'>
             advice #{id}
          </p>
          <blockquote>
-            <p className='text-2xl'>" {advice} "</p>
+            <p className='text-2xl text-darkGrayishBlue dark:text-lightCyan'>
+               " {advice} "
+            </p>
          </blockquote>
          <img src={divider} alt='divider' className='pb-4' />
 
          <button
             onClick={fetchAdvice}
-            className='cursor-pointer w-12 h-12 bg-neonGreen flex items-center justify-center rounded-full absolute left-1/2 -translate-x-1/2 bottom-0 translate-y-1/2 diceBox'
+            className='cursor-pointer w-12 h-12 bg-white shadow-sm border-2 dark:border-0 dark:border-transparent border-neutral-100 dark:bg-neonGreen flex items-center justify-center rounded-full absolute left-1/2 -translate-x-1/2 bottom-0 translate-y-1/2 dark:hover:neon-green-shadow'
             disabled={loading}
          >
             <img
                src={dice}
                alt='dice'
-               className={`w-5 ${loading ? 'animate-spin' : ''}`}
+               className={`w-5  ${loading ? 'animate-spin' : ''}`}
             />
          </button>
       </div>
